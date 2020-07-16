b08cd2c31edb287b473af5bd52a19edfb7111ad0
Sean <seand7565@gmail.com>
2020-07-16 09:30:46 -0500

    initial commit

---
+++ db/migrate/20200526123251_add_paypal_commerce_platform_sources.solidus_paypal_commerce_platform.rb
@@ -0,0 +1,12 @@
+# This migration comes from solidus_paypal_commerce_platform (originally 20200521190038)
+class AddPaypalCommercePlatformSources < ActiveRecord::Migration[6.0]
+  def change
+    create_table :paypal_commerce_platform_sources do |t|
+      t.string :paypal_order_id
+      t.integer :payment_method_id
+      t.string :authorization_id
+      t.string :capture_id
+      t.timestamps
+    end
+  end
+end
